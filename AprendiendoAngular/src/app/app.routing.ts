51b39b36e1900607689e3b79471814be40ca9b02
Alexjaca <jalexcardenasa@gmail.com>
2023-01-04 22:24:59 +0100

    Componentes y Rutas ANGULAR

--- AprendiendoAngular/src/app/app.routing.ts
+++ AprendiendoAngular/src/app/app.routing.ts
@@ -11,6 +11,7 @@ import { FormularioComponent } from "./components/formulario/formulario.componen
 import { PeliculasComponent } from "./components/peliculas/peliculas.component";
 import { PaginaComponent } from "./components/pagina/pagina.component";
 import { PruebasComponent } from "./components/pruebas/pruebas.component";
+import { ErrorComponent } from "./components/error/error.component";
 
 
 //Array de rutas-----------------------------------------------------------------
@@ -21,7 +22,9 @@ const appRoutes: Routes = [
     {path: 'blog', component: BlogComponent},
     {path: 'formulario', component: FormularioComponent},
     {path: 'peliculas', component: PeliculasComponent},
-    {path: 'pagina-de-pruebas', component: PruebasComponent}
+    {path: 'pagina-de-pruebas', component: PruebasComponent},
+//Importarte esta ruta debe agregarsae de ultima  para que funcionen las demas '**' es cuando la ruta no existe
+    {path: '**', component: ErrorComponent}
 ];
 
 //Exportar el modulo de rutas-----------------------------------------------------
